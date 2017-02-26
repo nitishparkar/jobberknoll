99b98cf4de070601f9e8d1694aa600ce6ea85fa6
Nitish Parkar <nitish@getsimpl.com>
2017-02-26 13:49:59 +0530

    Get database url from the environment

--- models/db.go
+++ models/db.go
@@ -3,10 +3,11 @@ package models
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"os"
 )
 
 func getDbConnection() (*gorm.DB, error) {
-	db, err := gorm.Open("postgres", "user=nitish dbname=jobberknoll_development sslmode=disable")
+	db, err := gorm.Open("postgres", os.Getenv("JOBBERKNOLL_DB_URL"))
 
 	return db, err
 }
